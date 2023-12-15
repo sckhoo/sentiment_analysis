c5e06f83d20ff7b4ba0986a25b29dc7ebedb9c7a
sckhoo <sckhoo@gmail.com>
2023-12-15 17:14:54 +0800

    initial code

---
+++ read_news.py
@@ -0,0 +1,27 @@
+from newsapi import NewsApiClient
+from sent import analyze_sentiment
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+NEWSAPI = os.environ.get("NEWSAPI")
+
+
+# Init
+newsapi = NewsApiClient(api_key=NEWSAPI)
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(category='general',
+                                          country='my',
+                                          language='en')
+
+if top_headlines['totalResults'] != 0:
+    #print(top_headlines['totalResults'])
+    for article in top_headlines['articles']:
+        print(f"Headline: {article['title']}")
+        analyze_sentiment(article['title'])
+else:
+    print("no news")
+
+
+
---
+++ sent.py
@@ -0,0 +1,29 @@
+from google.cloud import language_v2
+
+
+def analyze_sentiment(text_content: str = "I am so happy and joyful.") -> None:
+    """
+    Analyzes Sentiment in a string.
+
+    Args:
+      text_content: The text content to analyze.
+    """
+
+    client = language_v2.LanguageServiceClient()
+
+    document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
+
+    language_code = "en"
+    document = {
+        "content": text_content,
+        "type_": document_type_in_plain_text,
+        "language_code": language_code,
+    }
+
+    encoding_type = language_v2.EncodingType.UTF8
+
+    response = client.analyze_sentiment(
+        request={"document": document, "encoding_type": encoding_type}
+    )
+    # Get overall sentiment of the input document
+    print(f"score: {response.document_sentiment.score}, magnitude: {response.document_sentiment.magnitude}")
